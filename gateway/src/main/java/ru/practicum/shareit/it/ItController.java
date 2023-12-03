5ebdcce427176ac6f2b651927893c183b52df282
ds111166 <d.a.shevelev@yandex.ru>
2023-12-03 22:44:43 +0300

    style:

--- gateway/src/main/java/ru/practicum/shareit/it/ItController.java
+++ gateway/src/main/java/ru/practicum/shareit/it/ItController.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.it;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
